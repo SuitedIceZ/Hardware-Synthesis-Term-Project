f6b0ee05a4dc9ee18f9840176a968c7e1d84d4e4
SuitedIceZ <okshopm575@gmail.com>
2022-12-06 13:19:35 +0700

    Project initialize

---
+++ VivadoProject/VivadoProject.srcs/sources_1/new/mainSystem.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2022 01:11:02 PM
+// Design Name: 
+// Module Name: mainSystem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mainSystem(
+    input clk,
+    input RsRx,
+    output RsTx
+    );
+    
+    
+endmodule
